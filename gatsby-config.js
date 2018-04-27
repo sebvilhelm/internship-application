aaf727884a9eada0f7f24ae030c3ef5be411089e
Sebastian Vilhelm Nielsen <seb@frankly.dk>
2018-04-27 14:00:52 +0200

    add tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'YOUR_GOOGLE_ANALYTICS_TRACKING_ID',
+        trackingId: 'UA-83181202-2',
         // Puts tracking script in the head instead of the body
         head: false,
         // Setting this parameter is optional
