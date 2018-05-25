6c76990bc4e162362caa2fd7bc07a149bf65076b
VilhelmNielsen <seb.vilhelm@gmail.com>
2018-05-25 13:06:30 +0200

    upgrade react and everything else

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
     'gatsby-plugin-styled-components',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
+    'gatsby-plugin-react-next',
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
